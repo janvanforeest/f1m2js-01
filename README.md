152ae4cd5ec1bbd4185f8d8b3aced5d640737d0c
jan van foreest <30220@ma-web.nl>
2022-01-24 15:33:40 +0100

    javascriptreadme2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 "# f1m2js-01" 
 
-(live_view)[http://30220.hosts1.ma-cloud.nl/js-01-object-model/]
+[live_view](http://30220.hosts1.ma-cloud.nl/js-01-object-model/)
+[live_view](http://30220.hosts1.ma-cloud.nl/js-02-sortering/sortering/)
---
+++ js-object/script.js
@@ -0,0 +1,10 @@
+const kat= {
+    naam: "poepie",
+    leeftijd: 1 ,
+    hobbies:[ "geaaid worden", "slaapen", "miauwen"],
+    lengte_cm: 10,
+    kleur: "wit",
+    foto:"img/330px-VAN_CAT.png"
+}
+const myimg = document.getElementById("myimg");
+myimg.src = kat.foto;
